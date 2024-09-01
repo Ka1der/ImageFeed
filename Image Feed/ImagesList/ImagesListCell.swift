72af566c1c60206bfe2bbd9bba03b23660249660
Kaider <kronoz@yandex.ru>
2024-09-01 19:19:50 +0300

    Сверстана ячейка

---
+++ Image Feed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  Image Feed
+//
+//  Created by Kaider on 01.09.2024.
+//
+
+import Foundation
+
+final class ImagesListCell: UITableViewCell {
+    
+}
---
+++ Image Feed/ImagesListCell.swift
@@ -0,0 +1,8 @@
+//
+//  ImagesListCell.swift
+//  Image Feed
+//
+//  Created by Kaider on 01.09.2024.
+//
+
+import Foundation
