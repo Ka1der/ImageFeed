d96d3eaf3a90bccd530ac94a3b271264d539b682
Kaider <kronoz@yandex.ru>
2024-09-01 16:37:16 +0300

    Добавлен launchScreen и таблица

--- Image Feed/ImagesListViewController.swift
+++ Image Feed/ImagesListViewController.swift
@@ -5,7 +5,6 @@ final class ImagesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }
 
