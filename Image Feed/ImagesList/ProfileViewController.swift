acbba4e4b113d6a27271524fe9a9df59737256a3
Kaider <kronoz@yandex.ru>
2024-09-03 22:27:02 +0300

    Сверстал окно ЛК и добавил outlet'ы и action'ы

--- Image Feed/ImagesList/ProfileViewController.swift
+++ Image Feed/ImagesList/ProfileViewController.swift
@@ -5,4 +5,14 @@
 //  Created by Kaider on 03.09.2024.
 //
 
-import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet var LogOutButton: UIButton!
+    @IBOutlet var Avatar: UIImageView!
+    @IBOutlet var NickName: UILabel!
+    @IBOutlet var Adress: UILabel!
+    @IBOutlet var Status: UILabel!
+    
+}
