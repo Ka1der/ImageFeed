6b0c9485d0509a7087b9115e0d7de0bd77a9667a
Kaider <kronoz@yandex.ru>
2024-09-23 22:51:02 +0300

    Реализовано обновление в прогрессе загрузки веб-страницы через KVO

--- Image Feed/Auth/WebViewViewController.swift
+++ Image Feed/Auth/WebViewViewController.swift
@@ -7,52 +7,77 @@
 import WebKit
 import UIKit
 
+fileprivate let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+
+protocol WebViewViewControllerDelegate: AnyObject {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController)
+}
+
 final class WebViewViewController: UIViewController {
+    @IBOutlet var webView: WKWebView!
+    @IBOutlet var UIProgressView: UIProgressView!
     
     weak var delegate: WebViewViewControllerDelegate?
     
-    @IBOutlet var webView: WKWebView!
-    
-    @IBAction func didTapBackButton(_ sender: Any?) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    enum WebViewConstants {
-        static let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadAuthView()
         webView.navigationDelegate = self
-    }
-    
-    private func loadAuthView() {
-        guard var urlComponents = URLComponents(string: WebViewConstants.UnsplashAuthorizeURLString) else {
-            return
-        }
         
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
         urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: Constants.accessKey),
-            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "client_id", value: Constants.AccessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.RedirectURI),
             URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: Constants.accessScope)
+            URLQueryItem(name: "scope", value: Constants.AccessScope)
         ]
-        
-        guard let url = urlComponents.url else {
-            return
-        }
-        
+        let url = urlComponents.url!
         
         let request = URLRequest(url: url)
         webView.load(request)
         
+        updateProgress()
     }
     
+    @IBAction func didTapBackButton(_ sender: Any?) {
+        delegate?.webViewViewControllerDidCancel(self)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        webView.addObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            options: .new,
+            context: nil)
+        updateProgress()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            updateProgress()
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+    
+    private func updateProgress() {
+        UIProgressView.progress = Float(webView.estimatedProgress)
+        UIProgressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    }
+}
+
+extension WebViewViewController: WKNavigationDelegate {
     func webView(
         _ webView: WKWebView,
         decidePolicyFor navigationAction: WKNavigationAction,
-    decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
     ) {
         if let code = code(from: navigationAction) {
             //TODO: pocess code
@@ -75,9 +100,4 @@ final class WebViewViewController: UIViewController {
             return nil
         }
     }
-    
 }
-
-extension WebViewViewController: WKNavigationDelegate {
-    }
-
--- Image Feed/Constants.swift
+++ Image Feed/Constants.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 enum Constants {
-    static let accessKey = "mAt5FYVyrv2SvvSyk98OZZmsqj--NFNLxY8J9AY1vCU"
-    static let secretKey = "unhxiPE33mKCiGLx42T9Gz6NxVRCBJ50VyoBZV3dQLE"
-    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let AccessKey = "mAt5FYVyrv2SvvSyk98OZZmsqj--NFNLxY8J9AY1vCU"
+    static let SecretKey = "unhxiPE33mKCiGLx42T9Gz6NxVRCBJ50VyoBZV3dQLE"
+    static let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let AccessScope = "public+read_user+write_likes"
+    static let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
 }
 
 
