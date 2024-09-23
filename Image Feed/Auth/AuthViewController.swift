f2c2a5159f5a62664ed5b57f41839384f1b0a4c9
Kaider <kronoz@yandex.ru>
2024-09-23 13:04:21 +0300

    Добавил WebView  и кнопку back

---
+++ Image Feed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  Image Feed
+//
+//  Created by Kaider on 23.09.2024.
+//
+
+import Foundation
+
+final class AuthViewController: UIViewController {
+    
+}
---
+++ Image Feed/Auth/WebViewViewController.swift
@@ -0,0 +1,8 @@
+//
+//  WebViewViewController.swift
+//  Image Feed
+//
+//  Created by Kaider on 23.09.2024.
+//
+
+import Foundation
