2aa96bb94a7f48bdb62baa5c720e48d709613aea
Kaider <kronoz@yandex.ru>
2024-10-03 10:17:03 +0300

    Экан с кнопкой "Вход" открывается на весь экран

--- Image Feed/Auth/WebViewViewController.swift
+++ Image Feed/Auth/WebViewViewController.swift
@@ -24,25 +24,8 @@ final class WebViewViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         webView.navigationDelegate = self
-        
-        func createURLComponents() {
-            guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
-                return
-            }
-            urlComponents.queryItems = [
-                URLQueryItem(name: "client_id", value: Constants.accessKey),
-                URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-                URLQueryItem(name: "response_type", value: "code"),
-                URLQueryItem(name: "scope", value: Constants.accessScope)
-            ]
-            
-            guard let url = urlComponents.url else {
-                return
-            }
-            let request = URLRequest(url: url)
-            webView.load(request)
-            updateProgress()
-        }
+        updateProgress()
+        createURLComponents()
     }
     
     // MARK: - IBActions
@@ -78,6 +61,24 @@ final class WebViewViewController: UIViewController {
     private func updateProgress() {
         UIProgressView.progress = Float(webView.estimatedProgress)
         UIProgressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    }
+    
+    func createURLComponents() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
+            return
+        }
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        
+        guard let url = urlComponents.url else {
+            return
+        }
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
 }
 
