d3182f438f4f424e7917d39522e81f97bda57ac6
Kaider <kronoz@yandex.ru>
2024-10-20 16:53:43 +0300

    Обновлен метод  func objectTask<T: Decodable> в URLSession

--- Image Feed/Extension/URLSession+data.swift
+++ Image Feed/Extension/URLSession+data.swift
@@ -45,31 +45,68 @@ extension URLSession {
     }
     
     // Сетевой запрос данных пользователя с декодированием
-    func objectTask<T: Decodable>(for request: URLRequest, completion: @escaping (Result<T, Error>) -> Void) -> URLSessionTask {
-        
-        let fulfillCompletionOnTheMainThread: (Result<T, Error>) -> Void = { result in // Выполнение в главном потоке
-            DispatchQueue.main.async {
-                completion(result)
-            }
+//    func objectTask<T: Decodable>(for request: URLRequest, completion: @escaping (Result<T, Error>) -> Void) -> URLSessionTask {
+//        
+//        let fulfillCompletionOnTheMainThread: (Result<T, Error>) -> Void = { result in // Выполнение в главном потоке
+//            DispatchQueue.main.async {
+//                completion(result)
+//            }
+//        }
+//        
+//        let decoder = SnakeCaseJSONDecoder() // Декодер snake_case в camelCase
+//        
+//        let task = data(for: request) { (result: Result<Data, Error>) in
+//            switch result {
+//            case .success(let data):
+//                do {
+//                    let response = try decoder.decode(T.self, from: data)
+//                    fulfillCompletionOnTheMainThread(.success(response))
+//                } catch {
+//                    print("URLSession+data: Ошибка декодирования \(error.localizedDescription)") // Лог ошибок
+//                    print("URLSession+data: Данные ответа \(String(data: data, encoding: .utf8) ?? "N/A")")
+//                    fulfillCompletionOnTheMainThread(.failure(error))
+//                } case .failure(let error):
+//                print("URLSession+data: Ошибка сети \(error.localizedDescription)") // Лог ошибок
+//                fulfillCompletionOnTheMainThread(.failure(error))
+//            }
+//        }
+//        return task
+//    }
+    
+    func objectTask<T: Decodable>(
+            for request: URLRequest,
+            completion: @escaping (Result<T, Error>) -> Void
+        ) -> URLSessionTask {
+//            let decoder = JSONDecoder()
+            let decoder = SnakeCaseJSONDecoder() // Декодер snake_case в camelCase
+            let task = data(for: request) { (result: Result<Data, Error>) in
+                switch result {
+                       case .success(let data):
+                           do {
+                               // Декодирование данных в тип T
+                               let response = try decoder.decode(T.self, from: data)
+                               // Выполнение завершения на главном потоке
+                               DispatchQueue.main.async {
+                                   completion(.success(response))
+                               }
+                           } catch {
+                               // Логирование ошибки декодирования
+                               print("URLSession+data: Ошибка декодирования \(error.localizedDescription)") // Лог ошибок
+                               print("URLSession+data: Данные ответа \(String(data: data, encoding: .utf8) ?? "N/A")")
+                               // Завершение с ошибкой декодирования
+                               DispatchQueue.main.async {
+                                   completion(.failure(error))
+                               }
+                           }
+                       case .failure(let error):
+                           // Логирование сетевой ошибки
+                           print("URLSession+data: Ошибка сети \(error.localizedDescription)") // Лог ошибок
+                           // Завершение с сетевой ошибкой
+                           DispatchQueue.main.async {
+                               completion(.failure(error))
+                           }
+                       }
+                   }
+                   return task
         }
-        
-        let decoder = SnakeCaseJSONDecoder() // Декодер snake_case в camelCase
-        
-        let task = data(for: request) { (result: Result<Data, Error>) in
-            switch result {
-            case .success(let data):
-                do {
-                    let response = try decoder.decode(T.self, from: data)
-                    fulfillCompletionOnTheMainThread(.success(response))
-                } catch {
-                    print("URLSession+data: Ошибка декодирования \(error.localizedDescription)") // Лог ошибок
-                    print("URLSession+data: Данные ответа \(String(data: data, encoding: .utf8) ?? "N/A")")
-                    fulfillCompletionOnTheMainThread(.failure(error))
-                } case .failure(let error):
-                print("URLSession+data: Ошибка сети \(error.localizedDescription)") // Лог ошибок
-                fulfillCompletionOnTheMainThread(.failure(error))
-            }
-        }
-        return task
     }
-}
